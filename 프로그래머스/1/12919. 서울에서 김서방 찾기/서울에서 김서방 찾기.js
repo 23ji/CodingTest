308f7abeec0bc057fff1306f7730b3f9ba6defa8
23ji <85622819+23ji@users.noreply.github.com>
2024-04-29 08:45:44 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,11 +1,7 @@
 function solution(seoul) {
     var answer = 0;
     
-    for(var i = 0; i < seoul.length; i++){
-        if(seoul[i] === 'Kim'){
-            answer = i;
-            break;
-        }
-    }
-    return "김서방은 "+ answer +"에 있다";
+    var idx = seoul.indexOf('Kim');
+    
+    return "김서방은 "+ idx +"에 있다";
 }
