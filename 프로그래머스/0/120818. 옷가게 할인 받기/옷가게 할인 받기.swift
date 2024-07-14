fc0249285f5027464084d0ab81c4c89575754ce2
23ji <85622819+23ji@users.noreply.github.com>
2024-07-14 16:02:11 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
@@ -3,11 +3,11 @@ import Foundation
 func solution(_ price:Int) -> Int {
     
     if price >= 500000{
-        return Int(Double(price) * Double(0.8))
+        return Int(Double(price) * 0.8)
     }else if price >= 300000{
-        return Int(Double(price) * Double(0.9))
+        return Int(Double(price) * 0.9)
     }else if price >= 100000{
-        return Int(Double(price) * Double(0.95))
+        return Int(Double(price) * 0.95)
     }else {
         return price
     }
