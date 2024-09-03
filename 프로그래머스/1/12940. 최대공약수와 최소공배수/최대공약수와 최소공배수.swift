11ba390e2cc2c9e959610777f5d07140127c7b95
23ji <85622819+23ji@users.noreply.github.com>
2024-09-03 20:41:58 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -1,23 +1,9 @@
 func solution(_ n:Int, _ m:Int) -> [Int] {
-  
-    var min = 0
-    var max = 0
-    var minNum = n < m ? m : n
-    var maxNum = n < m ? m : n
-    for i in 1...maxNum{
-        if n % i == 0 && m % i == 0{
-            max = i
-        }
-    }
-    
-    for i in 1...minNum{
-        for j in 1...minNum{
-            if n * i == m * j{
-                dump(n*i)
-                min = m * j
-                return [max, min]
-            }
-        }
-    }
-    return [max, min]
+    var gcdNum = gcd(n, m)
+    var lcmNum = n * m / gcd(n, m)
+    return [gcdNum, lcmNum]
+}
+
+func gcd(_ n: Int, _ m: Int) -> Int {
+    return m == 0 ? n : gcd(m, n % m)
 }
