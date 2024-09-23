970ce297d2778037fc2f51f5fed1c782c3ce3d9c
23ji <85622819+23ji@users.noreply.github.com>
2024-09-23 14:35:44 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.77 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -2,14 +2,12 @@ import Foundation
 
 func solution(_ A:[Int], _ B:[Int]) -> Int
 {
-    var ans = 0
-    var sortA = A.sorted() // 1, 2, 4
-    var sortB = B.sorted(by: >) // 5, 4, 4
-    
-    var sum = 0
+    var sortA = A.sorted()
+    var sortB = B.sorted(by: >)
+    var result = 0
     
     for i in 0..<A.count{
-        sum += sortA[i] * sortB[i]
+        result += sortA[i] * sortB[i]
     }
-    return sum
+    return result
 }
