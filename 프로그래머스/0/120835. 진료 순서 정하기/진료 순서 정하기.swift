efe530f29e8f703a2c094483e61f04155e5c8740
23ji <85622819+23ji@users.noreply.github.com>
2024-11-28 21:42:31 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.swift
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.swift
@@ -1,18 +1,10 @@
 import Foundation
 
 func solution(_ emergency:[Int]) -> [Int] {
+    var arr = emergency
+    var sortNum: [Int] = arr.sorted(by: >)
     
-    // emergency 배열의 인덱스를 기준으로 내림차순 정렬된 배열 생성
-    let sortedEmergency = emergency.sorted(by: >)
+    var result = arr.map { sortNum.firstIndex(of: $0)!}.map { $0 + 1 }
     
-    // 응급도 순서를 저장할 배열
-    var rankArray = [Int](repeating: 0, count: emergency.count)
-    
-    // sortedEmergency의 각 값의 순위를 rankArray에 저장
-    for (index, value) in sortedEmergency.enumerated() {
-        if let originalIndex = emergency.firstIndex(of: value) {
-            rankArray[originalIndex] = index + 1
-        }
-    }
-    return rankArray
+    return result
 }
