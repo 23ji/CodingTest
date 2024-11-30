7a8995ab95773987294bc187f2a12e57abb12f05
23ji <85622819+23ji@users.noreply.github.com>
2024-11-30 20:48:54 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.19 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.swift
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.swift
@@ -1,17 +1,19 @@
-func solution(_ array: [Int]) -> Int {
-    var countDict = [Int: Int]()
-    
-    // 각 숫자의 출현 횟수를 세기
-    for num in array {
-        countDict[num, default: 0] += 1
+import Foundation
+
+func solution(_ array:[Int]) -> Int {
+   
+    var countDic = [Int: Int]()
+    for num in array{
+        countDic[num, default: 0] += 1
     }
+    //최대 횟수 찾기
+    let maxCount = countDic.values.max()
+    // 최빈 숫자 찾기
+    let maxNum = countDic.filter{ $0.value == maxCount }.keys
     
-    // 최대 출현 횟수 찾기
-    let maxCount = countDict.values.max() ?? 0
-    
-    // 최대 출현 횟수를 가진 숫자들 찾기
-    let mostFrequent = countDict.filter { $0.value == maxCount }.keys
-    
-    // 최빈값이 하나면 그 값을 반환, 여러 개면 -1 반환
-    return mostFrequent.count == 1 ? mostFrequent.first! : -1
+    if maxNum.count == 1 {
+        let result = (maxNum.first)!
+        return result
+    }
+    return -1
 }
