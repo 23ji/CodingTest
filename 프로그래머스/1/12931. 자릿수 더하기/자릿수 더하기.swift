c35fd3b326c0fb0356787a7b2c426fe648a2ef35
23ji <85622819+23ji@users.noreply.github.com>
2025-12-17 15:50:15 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -2,10 +2,14 @@ import Foundation
 
 func solution(_ n:Int) -> Int
 {
-    var arrNum = Array(String(n))
+    var stringN = String(n)
     var sum = 0
-    for i in arrNum{
-        sum += Int(String(i))!
+    var num = n
+
+    for i in 1...stringN.count {
+        sum += num % 10
+        num = (num - num % 10) / 10
     }
+    
     return sum
 }
